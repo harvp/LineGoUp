706a68fb01dcc13c9e47db505c2fa4d813054814
harvp <69948085+harvp@users.noreply.github.com>
2022-03-06 20:31:10 -0500

    Add Dummy Scripts for Twitter API

---
+++ APITest.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+import tweepy
+import json
+
+client = tweepy.Client(bearer_token="AAAAAAAAAAAAAAAAAAAAAHn2ZAEAAAAARCtdfQEvwI7L53ZL2oVUn0OW%2FEY%3DOB3SQ9Q9C8W5B8SRvXvrXRi5ztUm3TtDTQePdvlUew3x6efS2a")
+
+# Replace with your own search query
+user = "suhemparack"
+query = 'from:' + user + " -is:retweet"
+
+tweets = client.search_recent_tweets(query=query, tweet_fields=['context_annotations', 'created_at'], max_results=100)
+
+for tweet in tweets.data:
+    print(tweet.text)
+    tweetURL = "https://twitter.com/" + user + "/status/" + str(tweet.id)
+    print(tweetURL)
+    print(tweet.date)
+
+
---
+++ GetSingleTweetByAcc.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from pprint import pprint
+import json
+import os
+import datetime
+import operator
+import sys
+import getopt
+import tweepy
+
+if len(sys.argv) == 2:
+	print("https://twitter.com/suhemparack/status/1498390574910500865")
+else:
+	print("Error - invalid call")
