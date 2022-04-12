2c6c2a69d4847bc5776e1f5312842e03d64da7a7
Madeline E. Gesquiere <meg114@uakron.edu>
2022-04-11 20:41:49 -0400

    Edit python2.7 grabber

--- RecentByAccChron.php
+++ RecentByAccChron.php
@@ -2,10 +2,10 @@
 	<?php
 	
 	$name = "";
-	if(isset($_POST["name"])){
+	if(isset($_POST["user"])){
    		$name = $_POST["user"];
 	}
-	
+
 	$command = escapeshellcmd('python RecentByAccChron.py ' + $name);
 	$output = exec($command);
 	$counter = $output;
