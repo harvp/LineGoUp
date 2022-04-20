56711c9f02ae1ed10dc399cffa6ef793e8a2275d
Madeline E. Gesquiere <meg114@uakron.edu>
2022-04-19 22:37:30 -0400

    Edit format of volatility report

--- NumberCrunch.py
+++ NumberCrunch.py
@@ -171,7 +171,7 @@ volume_vi = numerator_volume_sum / (math.sqrt(denominator_volume_sum * denominat
 # print("The VI for the change in price is " + str(pct_vi))
 # print("The VI for the change in volume is " + str(volume_vi))
 
-outputString = "<h2> Volatility Report for " + company_name + "</h2><center><table cellpadding=5; border:1px solid; border-collapse:collapse;>"
+outputString = "<h2> Volatility Report for " + company_name + "</h2><center><table cellpadding=5; border:1px solid black; border-collapse:collapse;>"
 outputString += "<tr><th>Date</th><th>Price Change</th><th>Trade Volume</th><th>Twitter Engagement Score</th></tr>"
 i = 0
 for record in priceData:
