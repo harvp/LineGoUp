6347275cd77f8fe421efecde77730bc41f3cdaec
harvp <69948085+harvp@users.noreply.github.com>
2022-04-20 22:52:01 -0400

    Add batch file used to create database

---
+++ Filerunner.bat
@@ -0,0 +1,55 @@
+@echo off
+
+
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py abbottglobal	ABT
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py abercrombie	ANF
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py insperity	NSP
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py amd	AMD
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py aflac	AFL
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py agcocorp	AGCO
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py agilent	A
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py thrivent	TMSIX
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py AimcoApts	AIMCOPEST.BO
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py AirProducts	APD.MX
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py alaskaair	ALK
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py Unilever	UL
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py albertsons	ACI
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py allstate	ALL
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py hesscorporation	HES
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py uhaul	UHAL
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py AmerenCorp	AEE
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py americanair	AAL
+
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py americanexpress AXP
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py amwater AWK
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py amgen AMGN
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py Amica AMC.WA
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py amkortechnology AMKR
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py Aramark ARMK
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py ashlandinc ASH
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py atmosenergy ATO
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py autozone AZO
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py avaya AVYA
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py AveryDennison AVY
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py Avnet AVT
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py ballcorpHQ BLL
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py Chase JPM
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py beazerhomes BZH
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py biglots BIG
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py bjswholesale BJ
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py Boeing BA
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py BrownFormanJobs BF-B
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py Burlington BURL
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py conocophillips COP
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py CabotCorp CBT
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py cardinalhealth CAH
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py CarpenterTech CRS
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py CaseysGenStore CASY
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py crackerbarrel CBRL
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py ceridian CDAY
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py charlesschwab SCHW
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py Chevron CVX
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py cigna CI
+D:\Programs\Python\Python39\python.exe C:\wamp64\www\LineGoUp\NumberCrunch.py CintasCorp CTAS
+
+pause
